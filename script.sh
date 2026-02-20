3d0a870e91c2ebb295a2297e905e00f1bd3be5ea
WOLFI-VPN <it.isamirphone@gmail.com>
2026-02-20 20:28:45 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+#
+# ==================================================================================
+# 
+#                            WOLFI-VPN Script Launcher
+# 
+#  این اسکریپت به عنوان یک منوی مرکزی برای اجرای اسکریپت‌های مختلف عمل می‌کند.
+#  کاربران می‌توانند با انتخاب گزینه‌های مختلف، ابزارهای متفاوتی را اجرا کنند.
+#
+#  نگارش: 1.0.0
+#  توسعه‌دهنده: WOLFI-VPN
+#
+# ==================================================================================
+#
+
+# --- تعریف رنگ‌ها برای خروجی بهتر در ترمینال ---
 GREEN="\e[32m"
 BOLD_GREEN="\e[1;32m"
 YELLOW="\e[33m"
@@ -8,39 +24,59 @@ WHITE="\e[37m"
 RED="\e[31m"
 RESET="\e[0m"
 
-draw_green_line() {
-  echo -e "${GREEN}+--------------------------------------------------------+${RESET}"
-}
-
+# ------------------ تابع نمایش بنر گرافیکی ------------------
+# عملکرد: یک بنر زیبا با استفاده از کاراکترهای ASCII و رنگ‌های مختلف نمایش می‌دهد.
+#          همچنین اطلاعات توسعه‌دهنده و حامی مالی را چاپ می‌کند.
 print_art() {
-  echo -e "\033[1;32m                                                           "
-  echo -e "@@@@@@@   @@@@@@    @@@@@@                                 "
-  echo -e "@@@@@@@  @@@@@@@@  @@@@@@@@                                "
-  echo -e "  @@!    @@!  @@@  @@!  @@@                                "
-  echo -e "  !@!    !@!  @!@  !@!  @!@                                "
-  echo -e "  @!!    @!@!@!@!  @!@  !@!                                "
-  echo -e "  !!!    !!!@!!!!  !@!  !!!                                "
-  echo -e "  !!:    !!:  !!!  !!:!!:!:                                "
-  echo -e "  :!:    :!:  !:!  :!: :!:                                 "
-  echo -e "   ::    ::   :::  ::::: :!                                "
-  echo -e "   :      :   : :   : :  :::                               "
-  echo -e "@@@@@@@    @@@@@@    @@@@@@  @@@@@@@   @@@@@@   @@@  @@@   "
-  echo -e "@@@@@@@@  @@@@@@@@  @@@@@@@  @@@@@@@  @@@@@@@@  @@@@ @@@   "
-  echo -e "@@!  @@@  @@!  @@@  !@@        @@!    @@!  @@@  @@!@!@@@   "
-  echo -e "!@   @!@  !@!  @!@  !@!        !@!    !@!  @!@  !@!!@!@!   "
-  echo -e "@!@!@!@   @!@  !@!  !!@@!!     @!!    @!@!@!@!  @!@ !!@!   "
-  echo -e "!!!@!!!!  !@!  !!!   !!@!!!    !!!    !!!@!!!!  !@!  !!!   "
-  echo -e "!!:  !!!  !!:  !!!       !:!   !!:    !!:  !!!  !!:  !!!   "
-  echo -e ":!:  !:!  :!:  !:!      !:!    :!:    :!:  !:!  :!:  !:!   "
-  echo -e " :: ::::  ::::: ::  :::: ::     ::    ::   :::   ::   ::   "
-  echo -e ":: : ::    : :  :   :: : :      :      :   : :  ::    :    "
-  echo -e "                                                           \033[0m"
-  echo -e "\033[1;33m=========================================================="
-  echo -e "Developed by Parsa => https://github.com/ParsaKSH"
-  echo -e "\033[0m${RED}Sponsored by DigitalVPS.ir${RED}${RESET}"
-  echo -e "\033[1;33mLove Iran :)"
-  echo -e "\033[0m"
+  local art_width=62
+  local line_char="═"
+  local line=$(printf "%${art_width}s" | sed "s/ /${line_char}/g")
+
+  local art=$(cat << "EOF"
+@@@@@@@   @@@@@@    @@@@@@
+@@@@@@@  @@@@@@@@  @@@@@@@@
+  @@!    @@!  @@@  @@!  @@@
+  !@!    !@!  @!@  !@!  @!@
+  @!!    @!@!@!@!  @!@  !@!
+  !!!    !!!@!!!!  !@!  !!!
+  !!:    !!:  !!!  !!:!!:!:
+  :!:    :!:  !:!  :!: :!:
+   ::    ::   :::  ::::: :!
+   :      :   : :   : :  :::
+@@@@@@@    @@@@@@    @@@@@@  @@@@@@@   @@@@@@   @@@  @@@
+@@@@@@@@  @@@@@@@@  @@@@@@@  @@@@@@@  @@@@@@@@  @@@@ @@@
+@@!  @@@  @@!  @@@  !@@        @@!    @@!  @@@  @@!@!@@@
+!@   @!@  !@!  @!@  !@!        !@!    !@!  @!@  !@!!@!@!
+@!@!@!@   @!@  !@!  !!@@!!     @!!    @!@!@!@!  @!@ !!@!
+!!!@!!!!  !@!  !!!   !!@!!!    !!!    !!!@!!!!  !@!  !!!
+!!:  !!!  !!:  !!!       !:!   !!:    !!:  !!!  !!:  !!!
+:!:  !:!  :!:  !:!      !:!    :!:    :!:  !:!  :!:  !:!
+ :: ::::  ::::: ::  :::: ::     ::    ::   :::   ::   ::
+:: : ::    : :  :   :: : :      :      :   : :  ::    :
+EOF
+)
+  clear
+  echo -e "${CYAN}╔${line}╗${RESET}"
+  
+  while IFS= read -r art_line; do
+    printf "${CYAN}║${BOLD_GREEN}%*s%s%*s${CYAN}║${RESET}\n" $(((art_width - ${#art_line}) / 2)) "" "$art_line" $(((art_width - ${#art_line} + 1) / 2)) ""
+  done <<< "$art"
+
+  echo -e "${CYAN}╠${line}╣${RESET}"
+
+  local dev_line="Developed by WOLFI-VPN"
+  local sponsor_line="Sponsored by DigitalVPS.ir"
+  local love_line="♥ With Love From Iran ♥"
+
+  printf "${CYAN}║${YELLOW}%*s%s%*s${CYAN}║${RESET}\n" $(((art_width - ${#dev_line}) / 2)) "" "$dev_line" $(((art_width - ${#dev_line} + 1) / 2)) ""
+  printf "${CYAN}║${RED}%*s%s%*s${CYAN}║${RESET}\n" $(((art_width - ${#sponsor_line}) / 2)) "" "$sponsor_line" $(((art_width - ${#sponsor_line} + 1) / 2)) ""
+  printf "${CYAN}║${MAGENTA}%*s%s%*s${CYAN}║${RESET}\n" $(((art_width - ${#love_line}) / 2)) "" "$love_line" $(((art_width - ${#love_line} + 1) / 2)) ""
+  
+  echo -e "${CYAN}╚${line}╝${RESET}"
+  echo ""
 }
+# ------------------ تابع نمایش منوی اصلی ------------------
+# عملکرد: لیست گزینه‌های قابل انتخاب را برای کاربر نمایش می‌دهد.
 print_menu() {
   draw_green_line
   echo -e "${GREEN}|${RESET}              ${BOLD_GREEN}TAQ-BOSTAN Main Menu${RESET}                  ${GREEN}|${RESET}"
@@ -57,20 +93,23 @@ print_menu() {
   draw_green_line
 }
 
+# ------------------ تابع اجرای گزینه انتخابی ------------------
+# ورودی: شماره گزینه انتخاب شده توسط کاربر.
+# عملکرد: بر اساس شماره ورودی، دستور یا اسکریپت مربوطه را اجرا می‌کند.
 execute_option() {
   local choice="$1"
   case "$choice" in
     1)
       echo -e "${CYAN}Executing: Create best and safest tunnel...${RESET}"
-      bash <(curl -Ls https://raw.githubusercontent.com/ParsaKSH/TAQ-BOSTAN/main/hysteria.sh)
+      bash <(curl -Ls https://raw.githubusercontent.com/WOLFI-VPN/TAQ-BOSTAN/main/hysteria.sh)
       ;;
     2)
       echo -e "${CYAN}Executing: Create local IPv6 with Sit...${RESET}"
-      bash <(curl -Ls https://raw.githubusercontent.com/ParsaKSH/TAQ-BOSTAN/main/sit.sh)
+      bash <(curl -Ls https://raw.githubusercontent.com/WOLFI-VPN/TAQ-BOSTAN/main/sit.sh)
       ;;
     3)
       echo -e "${CYAN}Executing: Create local IPv6 with Wireguard...${RESET}"
-      bash <(curl -Ls https://raw.githubusercontent.com/ParsaKSH/TAQ-BOSTAN/main/wireguard.sh)
+      bash <(curl -Ls https://raw.githubusercontent.com/WOLFI-VPN/TAQ-BOSTAN/main/wireguard.sh)
       ;;
     4)
       echo -e "${CYAN}Deleting Hysteria tunnel...${RESET}"
@@ -116,8 +155,14 @@ execute_option() {
        fi
        ;;
      7)
-       read -p "For which foreign server number do you want to run the speedtest? " server_number
-       /usr/local/bin/hysteria -c /etc/hysteria/iran-config${server_number}.yaml speedtest
+       read -p "Enter the tunnel name to run the speedtest: " tunnel_name
+       if [ -z "$tunnel_name" ]; then
+         echo -e "${RED}Tunnel name cannot be empty.${RESET}"
+       elif [ ! -f "/etc/hysteria/iran-${tunnel_name}.yaml" ]; then
+         echo -e "${RED}Config file for tunnel '${tunnel_name}' not found.${RESET}"
+       else
+         /usr/local/bin/hysteria -c "/etc/hysteria/iran-${tunnel_name}.yaml" speedtest
+       fi
        ;;
      *)
        echo -e "${RED}Invalid option. Exiting...${RESET}"
